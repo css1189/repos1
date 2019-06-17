acc1473fc72f7b0c1c4dc7564755f6f5d1160d9c
css <css1189@163.com>
2019-06-17 18:05:50 +0800

    Merge branch 'master' of https://github.com/css1189/repos1
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- cvsxx/src/main/java/test.java
+++ cvsxx/src/main/java/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("1111112222222");
+        System.out.println("1111112222222");
+        System.out.println("1111112222222");
+        System.out.println("1111112222222");
     }
 }
